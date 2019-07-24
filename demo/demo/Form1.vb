e41af5d6c5b2e5391d882b7ce8d8ed2b804c0f46
kevin zhu <chenfeng1029@hotmail.com>
2019-07-24 23:29:46 +0800

    提交方式改为，调用link.ddsdk.httplib类库的postdata函数

--- demo/demo/Form1.vb
+++ demo/demo/Form1.vb
@@ -20,24 +20,24 @@ Public Class Form1
             }
         }
             Dim jsonRequest As String = JsonConvert.SerializeObject(json_req)
-            HttpPost(strMessageUrl, jsonRequest)
-
+         '   HttpPost(strMessageUrl, jsonRequest)
+            Httplib.PostData(strMessageUrl,jsonRequest)
         End If
     End Sub
-    Private Shared Function HttpPost(ByVal url As String, ByVal data As String) As String
-        Dim httpWebRequest As HttpWebRequest = CType(HttpWebRequest.Create(url), HttpWebRequest)
-        Dim bs As Byte() = Encoding.UTF8.GetBytes(data)
-        httpWebRequest.ContentType = "application/json"
-        httpWebRequest.ContentLength = bs.Length
-        httpWebRequest.Method = "POST"
-        httpWebRequest.Timeout = 20000
-        httpWebRequest.GetRequestStream().Write(bs, 0, bs.Length)
-        Dim httpWebResponse As HttpWebResponse = CType(httpWebRequest.GetResponse(), HttpWebResponse)
-        Dim streamReader As StreamReader = New StreamReader(httpWebResponse.GetResponseStream(), Encoding.UTF8)
-        Dim responseContent As String = streamReader.ReadToEnd()
-        streamReader.Close()
-        httpWebResponse.Close()
-        httpWebRequest.Abort()
-        Return responseContent
-    End Function
+'    Private Shared Function HttpPost(ByVal url As String, ByVal data As String) As String
+'        Dim httpWebRequest As HttpWebRequest = CType(HttpWebRequest.Create(url), HttpWebRequest)
+'        Dim bs As Byte() = Encoding.UTF8.GetBytes(data)
+'        httpWebRequest.ContentType = "application/json"
+'        httpWebRequest.ContentLength = bs.Length
+'        httpWebRequest.Method = "POST"
+'        httpWebRequest.Timeout = 20000
+'        httpWebRequest.GetRequestStream().Write(bs, 0, bs.Length)
+'        Dim httpWebResponse As HttpWebResponse = CType(httpWebRequest.GetResponse(), HttpWebResponse)
+'        Dim streamReader As StreamReader = New StreamReader(httpWebResponse.GetResponseStream(), Encoding.UTF8)
+'        Dim responseContent As String = streamReader.ReadToEnd()
+'        streamReader.Close()
+'        httpWebResponse.Close()
+'        httpWebRequest.Abort()
+'        Return responseContent
+'    End Function
 End Class
